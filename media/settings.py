04ade7adaf3ccd1888a8e95b83f8eb13f16e67c7
hazimal <hazimal@gmail.com>
2020-07-04 09:21:18 +0300

    change static files

--- media/settings.py
+++ media/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/home/hazem/PycharmProjects/psdmedia/media/static/',
+]
 STATIC_URL = '/static/'
